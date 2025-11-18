8f3f7de0dc6b663c9c1c65c027297a6c5912f6da
codinBabe <teboola2@gmail.com>
2025-11-18 12:12:03 +0100

    Feat: fix navitem map by hrefs

--- components/nav-items.tsx
+++ components/nav-items.tsx
@@ -15,7 +15,7 @@ const NavItems = ({
   return (
     <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
       {NAV_ITEMS.map((item) => {
-        if (item.label === "Search")
+        if (item.href === "/search")
           return (
             <li key="search-trigger">
               <SearchCommand
